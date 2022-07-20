35a91ec4437d2105d864a29907276ef573d081e6
Birivaty <105741142+Birivaty@users.noreply.github.com>
2022-07-20 19:13:47 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int result = 0;
+        if (cost >= 20) {
+            result = 1;
+        }
+        ;
+        return result;
+    }
 }
